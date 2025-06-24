023dfc55f1e094795ebb2c0f2a76c4407aee9208
Rein Subisol <subisolreineerkim@gmail.com>
2025-06-24 19:13:20 +0800

    feat: backend for viewing guides

--- backend/controllers/guide.user.controller.js
+++ backend/controllers/guide.user.controller.js
@@ -9,31 +9,55 @@ export const getGuides = async (req, res) => {
                     .sort({ createdAt: -1 })
                     .populate({
                         path: 'posterId',
-                        select: 'firstName lastName email profileIcon',});
+                        select: 'firstName lastName email profileIcon',
+                    });
                 return res.status(201).json({ success: true, message: `Guide fetched successfully.`, data: guides });
             } else {
                 const guides = await Guide.find({ status: 'accepted' })
                     .sort({ createdAt: -1 })
                     .limit(amount)
-                    .populate({path: 'posterId', select: 'firstName lastName email profileIcon',});
+                    .populate({ path: 'posterId', select: 'firstName lastName email profileIcon', });
                 return res.status(201).json({ success: true, message: `Guide fetched successfully.`, data: guides });
             }
         } else {
             if (!amount) {
                 const guides = await Guide.find({ status: 'accepted', category: category })
                     .sort({ createdAt: -1 })
-                    .populate({path: 'posterId', select: 'firstName lastName email profileIcon',});
+                    .populate({ path: 'posterId', select: 'firstName lastName email profileIcon', });
                 return res.status(201).json({ success: true, message: `Guide fetched successfully.`, data: guides });
             } else {
                 const guides = await Guide.find({ status: 'accepted', category: category })
                     .sort({ createdAt: -1 })
                     .limit(amount)
-                    .populate({path: 'posterId',select: 'firstName lastName email profileIcon',});
+                    .populate({ path: 'posterId', select: 'firstName lastName email profileIcon', });
                 return res.status(201).json({ success: true, message: `Guide fetched successfully.`, data: guides });
             }
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Something went wrong fetching guides." });
+        res.status(500).json({ success: false, message: 'Error fetching guides' })
     }
 };
+
+export const getGuide = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const guide = await Guide.findById(id).populate({
+            path: 'posterId',
+            select: 'firstName lastName email profileIcon'
+        });
+
+        if (!guide) {
+            return res.status(404).json({ success: false, message: 'Guide no longer exists.' });
+        }
+
+        if (guide.status !== 'accepted') {
+            return res.status(400).json({ success: false, message: 'Guide status not accepted.' });
+        } else {
+            return res.status(200).json({ success: true, message: `Guide fetched successfully.`, data: guide });
+        }
+    } catch (error) {
+        console.error('Error details:', error);
+        res.status(500).json({ success: false, message: 'Error fetching guide' });
+    }
+}
--- backend/routes/guide.user.route.js
+++ backend/routes/guide.user.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getGuides } from "../controllers/guide.user.controller.js";
+import { getGuides, getGuide } from "../controllers/guide.user.controller.js";
 
 const guideUserRouter = Router();
 
-guideUserRouter.get('/guides', getGuides)
+guideUserRouter.get('/guides/:id', getGuide);
+guideUserRouter.get('/guides', getGuides);
 
 export default guideUserRouter;
