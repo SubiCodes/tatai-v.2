e27aaa584c733fe23380b50c012649b0bbe71cf5
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-03 15:10:37 +0800

    feat: API for showing search results

--- backend/controllers/search.user.controller.js
+++ backend/controllers/search.user.controller.js
@@ -57,3 +57,59 @@ export const searchReccomendations = async (req, res) => {
         res.status(500).json({ success: false, message: "Server error" });
     }
 }
+
+export const searchResults = async (req, res) => {
+    const { search } = req.body;
+
+    if (!search) {
+        return res.status(404).json({ success: false, message: "No search found!" });
+    }
+
+    try {
+        const userMatches = await User.find({
+            $or: [
+                { firstName: { $regex: search, $options: 'i' } },
+                { lastName: { $regex: search, $options: 'i' } },
+            ],
+        }).limit(5);
+
+        const guideMatches = await Guide.find({
+            $or: [
+                { title: { $regex: search, $options: 'i' } },
+                { description: { $regex: search, $options: 'i' } },
+            ],
+            status: 'accepted'
+        }).limit(5);
+
+        const formattedUsers = userMatches.map(
+            (user) => ({type: user, data: user})
+        );
+
+        const formattedGuides = guideMatches.map(
+            (guide) => ({type: user, data: guide})
+        );
+
+        const combinedResults = [...formattedUsers, ...formattedGuides];
+        const q = search.toLowerCase();
+
+        combinedResults.sort((a, b) => {
+            const aLabel = a.toLowerCase();
+            const bLabel = b.toLowerCase();
+
+            const score = (label) => {
+                if (label === q) return 3;
+                if (label.startsWith(q)) return 2;
+                if (label.includes(q)) return 1;
+                return 0;
+            };
+
+            return score(bLabel) - score(aLabel);
+        });
+
+        return res.status(200).json({ success: true, message: "Successfully fetched data.", data: combinedResults });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+}
--- backend/routes/search.user.route.js
+++ backend/routes/search.user.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { searchReccomendations } from "../controllers/search.user.controller.js";
+import { searchReccomendations, searchResults } from "../controllers/search.user.controller.js";
 
 const searchUserRouter = Router();
 
 searchUserRouter.post('/reccomendations', searchReccomendations);
+searchUserRouter.post('/results', searchResults);
 
 export default searchUserRouter;
