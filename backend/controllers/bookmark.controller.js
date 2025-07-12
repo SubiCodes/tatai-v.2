4fb8aaf1efc2c90d838e760c4be384fd56d7cf61
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-12 16:18:25 +0800

    feat: created API for finding bookmarks by user

--- backend/controllers/bookmark.controller.js
+++ backend/controllers/bookmark.controller.js
@@ -23,3 +23,24 @@ export const createDestroyBookmark = async (req, res) => {
         return res.status(500).json({ success: false, message: 'Server Error' });
     }
 };
+
+
+export const findBookmark = async (req, res) => {
+    const { userId, guideId } = req.params;
+
+    if (!userId || !guideId) {
+        return res.status(400).json({ success: false, message: 'Missing parameters' });
+    }
+
+    try {
+        const bookmark = await Bookmark.findOne({ userId: userId, guideId: guideId });
+        if (!bookmark) {
+            return res.status(200).json({ success: true, message: 'Bookmark does not exist', exists: false });
+        } else {
+            return res.status(200).json({ success: true, message: 'Bookmark does not exist', exists: true });
+        }
+    } catch (error) {
+        console.log("Someting went wrong while looking for bookmark", error.message);
+        return res.status(500).json({ success: false, message: 'Server Error' });
+    }
+};
--- backend/routes/bookmark.route.js
+++ backend/routes/bookmark.route.js
@@ -3,6 +3,7 @@ import { createDestroyBookmark } from "../controllers/bookmark.controller.js";
 
 const bookmarkUserRouter = Router();
 
+bookmarkUserRouter.get('/:userId/:guideId')
 bookmarkUserRouter.post('/', createDestroyBookmark)
 
 export default bookmarkUserRouter
