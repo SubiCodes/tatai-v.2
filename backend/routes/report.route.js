6b8ce737a437e8daaa294dfa55bda67fed31400f
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-20 19:36:56 +0800

    fix: used the right import for changeReviewedStatus

--- backend/routes/report.route.js
+++ backend/routes/report.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createReport } from "../controllers/report.controller.js";
+import { changeReviewedStatus, createReport } from "../controllers/report.controller.js";
 
 const reportRouter = Router();
 
 reportRouter.post('/', createReport);
-reportRouter.put('/:id', createReport);
+reportRouter.put('/:id', changeReviewedStatus);
 
 export default reportRouter;
