8a31d8ab77b82769ae9726b6b1fe9905351ac4c7
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-20 19:21:20 +0800

    feat: connected the create report function to the router to complete the API

--- backend/routes/report.route.js
+++ backend/routes/report.route.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { createReport } from "../controllers/report.controller.js";
 
 const reportRouter = Router();
+
+reportRouter.post('/', createReport);
 
 export default reportRouter;
