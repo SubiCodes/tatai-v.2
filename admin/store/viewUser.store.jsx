c29c004a32c4330c8ad8043538dd50a32f9454fb
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-22 18:41:37 +0800

    feat: Created functions and state for view user data

--- admin/store/viewUser.store.jsx
+++ admin/store/viewUser.store.jsx
@@ -7,7 +7,26 @@ import ToastPending from "../src/components/util/ToastPending";
 import { toast } from "sonner";
 
 const useViewUserStore = create((set) => ({
-
+    userData: null,
+    fetchingUserData: false,
+    fetchUserData: async (id) => {
+        set({ fetchingUserData: true });
+        const toastId = toast.custom((t) => (
+            <ToastPending dismiss={() => toast.dismiss(t)} title={"Fetching user data"} message="This might take a while..." />));
+        try {
+            const res = await axios.get(`${import.meta.env.VITE_URI}/api/v1/viewUserAdmin/${id}`);
+            return res.data;
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+            toast.custom((t) => (
+                <ToastUnsuccessful dismiss={() => toast.dismiss(t)} title={"User Data Fetch Unsuccessful"} message={error.response.data.message} />
+            ));
+            return false;
+        } finally {
+            set({ fetchingUserData: false });
+            toast.dismiss(toastId);
+        }
+    }
 }));
 
 export default useViewUserStore;
