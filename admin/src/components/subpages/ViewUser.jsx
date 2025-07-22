efb5716058b96a295f66753e4c8ae82d089a6bb2
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-22 12:32:11 +0800

    feat: showing id params on main page

--- admin/src/components/subpages/ViewUser.jsx
+++ admin/src/components/subpages/ViewUser.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 function ViewUser() {
+
+    const { id } = useParams();
+
   return (
     <div>
-      ViewUser
+      ViewUser: {id}
     </div>
   )
 }
