8cd826c633d351f5b9ad5f9bd7781865d703aca7
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-22 18:39:11 +0800

    feat: created the store for viewing user

---
+++ admin/store/viewUser.store.jsx
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+import axios from "axios";
+
+import ToastUnsuccessful from "../src/components/util/ToastUnsuccessful";
+import ToastSuccessful from "../src/components/util/ToastSuccessful";
+import ToastPending from "../src/components/util/ToastPending";
+import { toast } from "sonner";
+
+const useViewUserStore = create((set) => ({
+
+}));
+
+export default useViewUserStore;
