574d12cd50e61f1295220941b6fc117ced14e76e
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-23 20:58:53 +0800

    feat: added header on reports page

--- admin/src/components/Pages/Reports.jsx
+++ admin/src/components/Pages/Reports.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 function Reports() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='w-full h-screen px-2 py-4 md:px-6 md:py-6 flex flex-col'>
+            {/* Header */}
+            <div className='w-full flex flex-row items-center mb-4'>
+                <h1 className='text-2xl text-primary font-bold'>Reports</h1>
+                <div className='flex flex-1' />
+            </div>
+        </div>
+    )
 }
 
 export default Reports
