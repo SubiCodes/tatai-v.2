1c32529c4811816d52bda8753f1709da877a00ba
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-25 14:18:47 +0800

    feat: added the function to get individual reports to route

--- backend/routes/report.route.js
+++ backend/routes/report.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { changeReviewedStatus, createReport, getReports } from "../controllers/report.controller.js";
+import { changeReviewedStatus, createReport, getReportById, getReports } from "../controllers/report.controller.js";
 
 const reportRouter = Router();
 
 reportRouter.get('/', getReports);
+reportRouter.get('/:id', getReportById);
 reportRouter.post('/', createReport);
 reportRouter.put('/:id', changeReviewedStatus);
 
