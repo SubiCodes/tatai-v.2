0b08793d4928e2ba7e50b53589af88e95ab5889e
Rein Subisol <subisolreineerkim@gmail.com>
2025-07-29 22:55:55 +0800

    feat: created route for fetching accepted guides data to tailored for feeding RAG bot data

--- backend/routes/chatbot.route.js
+++ backend/routes/chatbot.route.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getAcceptedGuidesData } from "../controllers/chatbot.controller.js";
 
 const chatbotRouter = Router();
+
+chatbotRouter.get('/data', getAcceptedGuidesData)
 
 export default chatbotRouter;
