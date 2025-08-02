ec62fd96b3932500b9d61af8cca182b93c8e3617
Rein Subisol <subisolreineerkim@gmail.com>
2025-08-02 16:43:56 +0800

    feat: made the button unclickable while uploading data

--- admin/src/components/Pages/AddData.jsx
+++ admin/src/components/Pages/AddData.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 
+import useChatbotStore from '../../../store/chatbot.store.jsx';
+
 function AddData() {
+    const { embeddingData, embedData } = useChatbotStore();
   return (
     <div className='w-full h-full flex items-center justify-center'>
-        <Button className="text-white">Update Chatbot</Button>
+        <Button className="text-white" disabled={embeddingData}>Update Chatbot</Button>
     </div>
   )
 }
