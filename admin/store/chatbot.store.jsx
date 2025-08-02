a5d491d9f6eb5d3401587696225789193e988816
Rein Subisol <subisolreineerkim@gmail.com>
2025-08-02 16:42:33 +0800

    feat: successfully created the store for posting data to chatbot

--- admin/store/chatbot.store.jsx
+++ admin/store/chatbot.store.jsx
@@ -10,7 +10,28 @@ import ToastPending from "../src/components/util/ToastPending.jsx";
 const URI = import.meta.env.VITE_URI;
 
 const useChatbotStore = create((set) => ({
-
+    embeddingData: false,
+    embedData: async () => {
+        set({ embeddingData: true });
+        const toastId = toast.custom((t) => (
+            <ToastPending dismiss={() => toast.dismiss(t)} title={"Uploading Data to TatAi Chatbot"} message="This might take a while..." />));
+        try {
+            const res = await axios.post(`${URI}/api/v1/chatbot/data`);
+            console.log(res.data);
+            toast.custom((t) => (
+                <ToastSuccessful dismiss={() => toast.dismiss(t)} title={"Upload Successful!"} message={res.data.message} />
+            ));
+        } catch (error) {
+            console.log("Error uploading data:", error);
+            toast.custom((t) => (
+                <ToastUnsuccessful dismiss={() => toast.dismiss(t)} title={"Uploading Data to TatAi Chatbot Failed"} message={error.response.data.message} />
+            ));
+            return false;
+        } finally {
+            set({ embeddingData: false });
+            toast.dismiss(toastId);
+        }
+    }
 }));
 
 export default useChatbotStore;
