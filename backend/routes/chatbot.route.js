ce2dbc09c5f638f873aeaebb02d2b381849cd597
Rein Subisol <subisolreineerkim@gmail.com>
2025-08-07 22:38:58 +0800

    feat: created the route for transcribing audio

--- backend/routes/chatbot.route.js
+++ backend/routes/chatbot.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { askChatbot, uploadGuidesToChatbot } from "../controllers/chatbot.controller.js";
+import { askChatbot, transcribeAudio, uploadGuidesToChatbot } from "../controllers/chatbot.controller.js";
 
 const chatbotRouter = Router();
 
 chatbotRouter.post('/data', uploadGuidesToChatbot);
 chatbotRouter.post('/ask', askChatbot)
+chatbotRouter.post('/transcribe', transcribeAudio);
 
 export default chatbotRouter;
