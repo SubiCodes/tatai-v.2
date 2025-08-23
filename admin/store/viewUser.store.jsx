e98d1dbbc85fafd09f56550269ff1f387cddbe57
Rein Subisol <subisolreineerkim@gmail.com>
2025-08-23 12:32:09 +0800

    fix: added a new field reason for the view user store

--- admin/store/viewUser.store.jsx
+++ admin/store/viewUser.store.jsx
@@ -35,11 +35,11 @@ const useViewUserStore = create((set, get) => ({
     getGuideByIdFromViewUser: (id) => {
         return get().guides.find((g) => g._id === id);
     },
-    updateGuideStatusFromViewUser: async (id, status) => {
+    updateGuideStatusFromViewUser: async (id, status, reason) => {
         const toastId = toast.custom((t) => (
             <ToastPending dismiss={() => toast.dismiss(t)} title={"Updating Guide Status"} message="This might take a while..." />));
         try {
-            const result = await axios.put(`${import.meta.env.VITE_URI}/api/v1/guideAdmin/guide/status/${id}`, { status: status });
+            const result = await axios.put(`${import.meta.env.VITE_URI}/api/v1/guideAdmin/guide/status/${id}`, { status: status, reason: reason });
             set((state) => ({ guides: state.guides.map((guide) => guide._id === id ? { ...guide, status } : guide) }));
             toast.custom((t) => (
                 <ToastSuccessful dismiss={() => toast.dismiss(t)} title={"Updating guide status Successful"} message={result.data.message} />
