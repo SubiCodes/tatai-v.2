c70da625ed86d5d5f7a0aeac678148cbf91904fa
Rein Subisol <subisolreineerkim@gmail.com>
2025-08-30 18:47:51 +0800

    feat: created a route for changing the user rols

--- backend/routes/user.admin.route.js
+++ backend/routes/user.admin.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { createUser, getUsers, updateStatus } from "../controllers/user.admin.controller.js";
+import { createUser, getUsers, updateRole, updateStatus } from "../controllers/user.admin.controller.js";
 
 const userAdminRouter = Router();
 
 userAdminRouter.get("/user", getUsers);
 userAdminRouter.post("/user", createUser);
 userAdminRouter.post("/status/:id", updateStatus);
+userAdminRouter.post("/role/:id", updateRole);
 
 export default userAdminRouter;
