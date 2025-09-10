682b6c741c8a99867c73d18f5075c57932fd24bc
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-10 20:37:28 +0800

    feat: created a function for creating new instances of notifications

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,15 @@
+import Notification from "../models/notification.model.js";
+
+export const createNotification = async (userId, type, display, title, message) => {
+    if (!userId || !type || !display || !title || !message) {
+        return;
+    }
+    try {
+        const newNotification = new Notification({ userId, type, display, title, message });
+        await newNotification.save();
+        return (newNotification);
+    } catch (error) {
+        console.error("Error creating notification:", error);
+        throw error;
+    }
+} 
