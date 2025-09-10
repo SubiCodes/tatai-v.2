87133d8da71d719a7dd84c0ed0b06747473e06c2
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-10 21:12:14 +0800

    feat: created controller for setting a notification as viewed.

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -12,4 +12,18 @@ export const createNotification = async (userId, type, display, title, message) 
         console.error("Error creating notification:", error);
         throw error;
     } 
-} 
+};
+
+export const viewNotification = async (req, res) => {
+    const { notificationId } = req.params;
+    if (!notificationId) { return res.status(400).json({ success: false, message: 'Notification ID missing. Cannot proceed to editing the viewed status.' }); }
+    try {
+        const notification = await Notification.findById(notificationId);
+        notification.viewed = true;
+        await notification.save();
+        return res.status(200).json({ success: true, message: 'Notification labeled as viewed.', data: {notification}});
+    } catch (error) {
+        console.error('Error on updating viewed status for notification:', error);
+        return res.status(500).json({ success: false, message: 'An unexpected error while updating the viewed status occured.' });
+    }
+};
