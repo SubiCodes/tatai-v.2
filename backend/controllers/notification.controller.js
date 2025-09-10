79fe0636fc3c57edc0db5e35c0302b0039adc81f
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-10 21:46:24 +0800

    feat: created an API for getting user notifications

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -14,6 +14,18 @@ export const createNotification = async (userId, type, display, title, message) 
     }
 };
 
+export const getNotifications = async (req, res) => {
+    const { userId } = req.params;
+    if (!userId) { return res.status(400).json({ success: false, message: 'User ID missing. Cannot proceed to fetching notifications.' }); }
+    try {
+        const notifications = await Notification.find({userId: userId}).sort({createdAt: -1});
+        return res.status(200).json({ success: true, message: 'Notifications fetched.', data: notifications });
+    } catch (error) {
+        console.error('Error on fetching notifications:', error);
+        return res.status(500).json({ success: false, message: 'An unexpected error while fetching notifications occured.' });
+    }
+};
+
 export const viewNotification = async (req, res) => {
     const { notificationId } = req.params;
     if (!notificationId) { return res.status(400).json({ success: false, message: 'Notification ID missing. Cannot proceed to editing the viewed status.' }); }
