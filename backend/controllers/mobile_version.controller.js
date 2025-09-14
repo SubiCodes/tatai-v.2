fec9b045d1c4022376204fa6f66f511ba84301fe
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-14 22:41:47 +0800

    feat: created a controller for udpating the version

--- backend/controllers/mobile_version.controller.js
+++ backend/controllers/mobile_version.controller.js
@@ -10,3 +10,33 @@ export const getCurrentMobileVersion = async (req, res) => {
         return res.status(500).json({ success: false, message: `Error in fetching latest version: ${error}` });
     }
 }
+
+export const updateMobileVersion = async (req, res) => {
+    const { data } = req.body;
+
+    try {
+        const existingVersion = await MobileVersion.findOne().sort({ createdAt: -1 });
+        if (existingVersion) {
+            existingVersion.version = data;
+            await existingVersion.save();
+            return res.status(200).json({
+                success: true,
+                message: "Current version for mobile updated successfully.",
+                data: existingVersion
+            });
+        } else {
+            const newVersion = await MobileVersion.create({ version: data });
+            return res.status(201).json({
+                success: true,
+                message: "New version for mobile created successfully.",
+                data: newVersion
+            });
+        }
+    } catch (error) {
+        console.error("Error in updating latest version.", error);
+        return res.status(500).json({
+            success: false,
+            message: `Error in updating latest version: ${error.message}`
+        });
+    }
+};
