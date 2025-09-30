67433ccbb5e43661e4419fb999dfa325cf4a4494
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-30 19:34:02 +0800

    feat: Created the schema for storing conversations with AI

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    messages: {
+        type: [{role: "user" | "model", message: String}],
+        required: true
+    }
+}, {
+    collection: "bookmark",
+    timestamps: true
+});
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+
+export default Bookmark;
