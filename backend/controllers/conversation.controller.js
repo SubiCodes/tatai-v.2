b9535a69dab9e96fb483251860def0a72458d794
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-30 19:51:06 +0800

    feat: Created a controller for fetching conversations

--- backend/controllers/conversation.controller.js
+++ backend/controllers/conversation.controller.js
@@ -33,10 +33,21 @@ export const updateConversation = async (req, res) => {
 export const deleteConversation = async (req, res) => {
     const { id } = req.params;
     try {
-        await Conversation.deleteOne({_id: id});
-        return res.status(200).json({ success: true, message: "Successfully deleted the conversation."});
+        await Conversation.deleteOne({ _id: id });
+        return res.status(200).json({ success: true, message: "Successfully deleted the conversation." });
     } catch (error) {
         console.log("Someting went wrong while deleting conversation", error.message);
+        return res.status(500).json({ success: false, message: 'Server Error' });
+    }
+};
+
+export const getConversations = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const conversations = await Conversation.find({userId: userId});
+        return res.status(200).json({ success: true, message: "Successfully fetched conversations.", data: conversations });
+    } catch (error) {
+        console.log("Someting went wrong while fetching conversations", error.message);
         return res.status(500).json({ success: false, message: 'Server Error' });
     }
 };
