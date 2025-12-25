855cc4e7f10e89ff45fd95e5a4ca75b27b62328f
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-25 15:33:31 +0800

    feat: OpenAI client updated

--- backend/utils/openaiClient.js
+++ backend/utils/openaiClient.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 export default openai;
